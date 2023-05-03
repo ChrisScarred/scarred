7e35ba74ee6e8d0619d1c33f2fd9ba0e1e029f5c
ChrisScarred <scarred.chris@gmail.com>
2023-05-03 22:43:28 +0200

    connection pool added, not tested

--- src/services/db_handler.py
+++ src/services/db_handler.py
@@ -1,12 +1,37 @@
+from typing import Any, Tuple
+
 import psycopg2
+from psycopg2.pool import ThreadedConnectionPool
+
 
 class Cockroach:
-    def __init__(self, url: str) -> None:
-        self.connection = psycopg2.connect(url)
+    def __init__(self, url: str, _min: int, _max: int) -> None:
+        self.cp = ThreadedConnectionPool(_min, _max, url)
     
+    def get(self, query: str, params: Tuple[Any] = ()) -> str:
+        res = None
+        conn = self.cp.getconn()
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute(query, params)
+                res = cur.fetchall()
+        self.cp.putconn(conn)
+        return res
+        
+    def insert(self) -> bool:
+        pass
+
     def test(self) -> str:
-        with self.connection.cursor() as cur:
-            cur.execute("SELECT now()")
-            res = cur.fetchall()
-            self.connection.commit()
-            return res
+        return self.get("SELECT now()")
+
+    def show_tables(self) -> str:
+        return self.get("SHOW TABLES")
+    
+    def create_table(self, name: str, desc: str) -> bool:
+        pass
+
+    def delete_row(self, condition: str) -> bool:
+        pass
+
+    def close(self) -> None:
+        self.cp.closeall()
