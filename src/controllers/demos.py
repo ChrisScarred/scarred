f60c84d8337043344ab3ea5383c2d502fa104e1e
ChrisScarred <scarred.chris@gmail.com>
2023-05-08 02:17:59 +0200

    wip dragon age

--- src/controllers/demos.py
+++ src/controllers/demos.py
@@ -1,8 +1,23 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Form
 from starlette.requests import Request
-from fastapi import Form, APIRouter
 
 from src.assets.dragon_age import respond
+from src.services.dependencies import context, routes
 
-router = APIRouter(prefix="/demos")
+router = APIRouter()
 
-
+@router.get(routes.get("demos.dragon_age"))
+@router.post(routes.get("demos.dragon_age"))
+async def dragon_age_demo(request: Request, species: Annotated[str, Form()], age: Annotated[int, Form()]):
+    method = request.method
+    if method == "GET":
+        return context.get("templates").TemplateResponse(
+            "dragon_age.html", context={"request": request, "title": "Dračí vek"}
+        )
+    elif method == "POST":
+        result = respond(species, age)
+        return context.get("templates").TemplateResponse(
+            "dragon_age.html", context={"request": request, "title": "Dračí vek", "result": result}
+        )
